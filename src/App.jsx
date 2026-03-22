c5a02cc3d3be88c77039b932b676f8ad798aa89a
jeetgithub1 <jeetkumar95232@gmail.com>
2026-03-23 00:56:24 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Character from "./components/Character";
+
+function App() {
+  return (
+    <div style={{ height: "100vh", background: "#000" }}>
+      <Character />
+    </div>
+  );
+}
+
+export default App;
